61f6af569394aa4bf498a51a3126560bbec458a9
ElBuenAnvita <37051987+ElBuenAnvita@users.noreply.github.com>
2018-03-24 22:47:21 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,12 @@ client.on('message', message => {
     if (message.content === '.ping') {
     	message.reply('**Pong!** :ping_pong:');
   	}
+client.on('message', message => {
+    if (message.content === '.help') {
+        message.reply('Mis comandos son: `.help` `.ping` `.info` Bot en desarrollo, creado por ElBuenAnvita.');
+    }
+});
+    bot.user.SetGame('.help')
 });
 
 // THIS  MUST  BE  THIS  WAY
