dffef102568ca1c71817549432551101385ecf06
ElBuenAnvita <37051987+ElBuenAnvita@users.noreply.github.com>
2018-04-25 13:36:29 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,6 +35,16 @@ client.on('message', message => {
   	}
 });
 
+// Create an event listener for new guild members
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find('name', 'member-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
+});
+
 client.on('message', message => {
     if (message.content === 'que') {
     	message.channel.send('so! :cheese: ');
