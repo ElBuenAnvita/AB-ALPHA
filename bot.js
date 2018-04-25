9ff2f3a2dba907d7e45609a6f5b8a3cd616851a1
ElBuenAnvita <37051987+ElBuenAnvita@users.noreply.github.com>
2018-04-25 13:42:23 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -51,7 +51,7 @@ client.on('guildMemberAdd', member => {
   // Do nothing if the channel wasn't found on this server
   if (!channel) return;
   // Send the message, mentioning the member
-  channel.send(`Welcome to the server, ${member}`);
+  channel.send(`¡Bienvenido al servidor, ${member}!`);
 });
 
 client.on('message', message => {
