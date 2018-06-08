5de4a16ddcd872a0cb58c1f6a4094bc4a86ca227
ElBuenAnvita <37051987+ElBuenAnvita@users.noreply.github.com>
2018-06-08 16:02:07 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,36 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = "--";
-  
+const InfiniteLoop = require('infinite-loop');
+const il = new InfiniteLoop;
+const quotes = ["Sí", "No", "No sé", "Buena pregunta, pero no sé la respuesta", "Nunca.", "Definitivamente sí", "Definitivamente no", "No pasará", "50/50", "No responderé eso", "Mmm... ahora ando descansando, prueba después"]
+
+function randomQuote() {
+	return quotes[Math.floor(Math.random() * quotes.length)];
+};
+il.add(randomQuote, []);
+
+il.run();
+
+console.log(randomQuote());
+//from here down is different gif for command --kiss ------------------------------------------------------
+const quotes2 = ["https://i.imgur.com/9y34cfo.gif", "https://media.giphy.com/media/FqBTvSNjNzeZG/giphy.gif", "https://media1.tenor.com/images/78095c007974aceb72b91aeb7ee54a71/tenor.gif?itemid=5095865", "https://media.giphy.com/media/ZRSGWtBJG4Tza/giphy.gif",]
+
+function randomQuote2() {
+	return quotes2[Math.floor(Math.random() * quotes2.length)];
+};
+il.add(randomQuote2, []);
+
+il.run();
+
+console.log(randomQuote2());
+
 client.on('ready', () => {
-  client.user.setGame(prefix + 'ayuda | Ayudando a ' + client.guilds.size + ' servidores, y sirviendo a ' + client.users.size + ' personitas n_n')
+  client.user.setGame('en actualizaciones, no usarme a menos que seas tester.')
 });
-  
+
 client.on("message", message => {
+  const version = "alpha_0.1.3.9(edit_19)";
   if (!message.content.startsWith(prefix)) return;
   
   if (message.content.startsWith(prefix + "ping")) {
@@ -16,28 +40,20 @@ client.on("message", message => {
     message.channel.send("xDD");
   } else
   if (message.content.startsWith(prefix + "ayuda")) {
-    message.channel.send("Soy un bot too' raro.\n¡Consulta mi lista de comandos con `--comandos`!");
+    message.channel.send("Soy un bot too' raro.\n¡Consulta mi lista de comandos con `__comandos`!");
   } else
   if (message.content.startsWith(prefix + "comandos")) {
     message.channel.send("Te envié mis comandos por mensaje privado, " + message.author.username);
-    message.author.send("```Comandos```\n**--ayuda**: Consigue ayuda del bot.\n**--info**: Información del bot\n**--me**: Consigue tu id de jugador.\n**--ping**: ¡Ping Pong! :ping_pong:\n**--8ball**: Efectua una pregunta <<Sí/no>> (en desuso)\n**--avatar**: Consulta la URL de tu avatar actual.\n**--invite**: Invita a AnviBot a tu servidor de Discord!\n**--xDD**: xDD\n```Respuestas```\n**ayy**: lmao\n**omae wa mou shindeiru**: NANI?!\n**k**, **ke**, **que**: so! :cheese: \n**ki**: ero pan :bread: \n```Página```\nhttp://anvibot.blogspot.com/p/commands");
+    message.author.send("```Comandos```\n**__ayuda**: Consigue ayuda del bot.\n**__info**: Información del bot\n**__me**: Consigue tu id de jugador.\n**__ping**: ¡Ping Pong! :ping_pong:\n**__avatar**: Consulta la URL de tu avatar actual.\n**__invite**: Invita a AnviBot a tu servidor de Discord!\n**__xDD**: xDD\n```Página```\nhttp://anvibot.blogspot.com/p/commands");
   } else
   if (message.content.startsWith(prefix + "info")) {
-    message.channel.send("Estado del Bot: **Conectado**\nFallos: **0**\nCreador: **ElBuenAnvita**\nVersión del Bot: **beta_1.1.5**");
+    message.channel.send("Estado del Bot: **Conectado**\nFallos: **0**\nCreador: **ElBuenAnvita**\nVersión del Bot: " + version);
   } else
   if (message.content.startsWith(prefix + "invite")) {
-    message.channel.send("```AnviBot Discord Bot```\nSi tienes dudas sobre el bot o quieres colaborar, puedes entrar con el siguiente enlace:\nhttps://discord.gg/PSFfWFp\n\n```Invita AnviBot a tu servidor de Discord```\nPuedes invitar al bot con el siguiente enlace:\nhttps://discordapp.com/api/oauth2/authorize?client_id=419980531564806145&permissions=8&scope=bot\n\n**¡Gracias por usar AnviBot n.n!**");
+    message.channel.send("```AnviBot Alpha Discord Bot```\nSi tienes dudas sobre el bot o quieres colaborar, puedes entrar con el siguiente enlace:\nhttps://discord.gg/PSFfWFp\n\n```Invita AnviBot a tu servidor de Discord```\nPuedes invitar al bot con el siguiente enlace:\nhttps://discordapp.com/api/oauth2/authorize?client_id=419980531564806145&permissions=8&scope=bot\n\n**¡Gracias por usar AnviBot n.n!**");
   }
-});
-
-client.on("message", message => {
-  if (!message.content.startsWith(prefix)) return;
-  
   if (message.content.startsWith(prefix + "8ball")) {
-    let replies = ["Sí", "No"];
-    let result = Math.floor((Math.random() * replies.lenght));
-    
-    message.channel.send('tal vez... ' + result);
+    message.reply(randomQuote());
   }
 });
 
@@ -48,19 +64,16 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-    if (message.content === 'prueba 1') {
-    	message.channel.send({ ejemplo });
-  	}
-});
-
-client.on('message', message => {
     if (message.content.startsWith(prefix + "kiss")) {
       const embed = {
         "title": "",
-        "description": "¿Besandote tú mism@, **" + message.author.username + "**? Perdón por eso.",
+        "description": "¿Besandote tú mism@, <@!" + message.author.id + ">? Perdón por eso.",
         "color": 2335,
+        "footer": {
+          "text": "Las imágenes pueden estar sujetas a derechos de autor"
+        },
         "image": {
-          "url": "https://media.giphy.com/media/ZRSGWtBJG4Tza/giphy.gif"
+          "url": randomQuote2()
         }
       }
       message.channel.send({ embed });
@@ -87,6 +100,9 @@ client.on('message', message => {
         "title": "",
         "description": "No te preocupes, **" + message.author.username + "**, yo te acaricio.",
         "color": 2335,
+        "footer": {
+          "text": "Las imágenes pueden estar sujetas a derechos de autor"
+        },
         "image": {
           "url": "http://2.bp.blogspot.com/-X0ZDtsVYvVk/WvOwG3Y1BYI/AAAAAAAABiQ/lt6TDbpSyXMoIglTBQH-JonovXCgB2QjACK4BGAYYCw/s1600/anime-head-pat-gif-4.gif"
         }
@@ -95,45 +111,4 @@ client.on('message', message => {
     }
 });
 
-// Create an event listener for new guild members
-client.on('guildMemberAdd', member => {
-  // Send the message to a designated channel on a server:
-  const channel = member.guild.channels.find('name', 'member-log');
-  // Do nothing if the channel wasn't found on this server
-  if (!channel) return;
-  // Send the message, mentioning the member
-  channel.send(`¡Bienvenido al servidor, ${member}!`);
-});
-
-client.on('message', message => {
-    if (message.content === 'que') {
-    	message.channel.send('so! :cheese: ');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === 'ayy') {
-    	message.channel.send('lmao');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === 'k') {
-    	message.channel.send('so! :cheese:');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === 'omae wa mou shindeiru') {
-    	message.channel.send('なに？！');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === 'ki') {
-    	message.channel.send('ero pan :bread:');
-  	}
-});
-
-// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
